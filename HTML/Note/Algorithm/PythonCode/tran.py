92fdbc0f8ba4920d21e9fc22cb200eceeab545f9
Fish-Ash <114203104+Fish-Ash@users.noreply.github.com>
2024-12-23 21:28:57 +0800

    2024-12-23

---
+++ HTML/Note/Algorithm/PythonCode/tran.py
@@ -0,0 +1,9 @@
+from math import *
+# line = [int(i) for i in input().split(' ')]
+c = [0.227, 0.204, 0.184]
+# c = [5, 4, 3]
+
+
+cosx = (c[1]**2 + c[2]**2 - c[0]**2)/(2*c[1]*c[2])
+print(acos(cosx))
+print(632.8/3)
