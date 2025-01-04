2ee0d5090afbe785a12b8e79f42d476a090e0bb6
Fish-Ash <114203104+Fish-Ash@users.noreply.github.com>
2025-01-04 09:41:52 +0800

    2025-01-04

---
+++ JavaScript/Scroll.js
@@ -0,0 +1,7 @@
+function ToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+}
+
+function ToBottom() {
+    window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' });
+}
---
+++ Test/UpDown/scripts.js
@@ -0,0 +1,7 @@
+function ToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+}
+
+function oBottom() {
+    window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' });
+}
